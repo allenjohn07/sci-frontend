3c3865766d467e40ab6de665ad807f8d319f24d4
Allen John <allenjohnmonapallil@gmail.com>
2025-03-10 18:14:52 +0530

    Updated Rankings

--- src/components/ranking.jsx
+++ src/components/ranking.jsx
@@ -11,7 +11,7 @@ export default function RankingsPage() {
   }, []);
 
   return (
-    <div className="container about_page_div px-4 md:px-8 py-8 md:py-14 text-blue-gray-900">
+    <div className="container about_page_div px-4 md:px-8 py-8 md:py-14 text-blue-gray-900 min-h-screen">
       <h1 className="mb-6 border-b md:text-center pb-2 text-3xl font-semibold tracking-tight first:mt-0">
         Rankings
       </h1>

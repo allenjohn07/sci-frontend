f1132d4838fd5e317ddd9ef40202d6038e5f4a30
Allen John <allenjohnmonapallil@gmail.com>
2025-03-24 21:51:36 +0530

    Alert as separate component

--- src/components/ranking.jsx
+++ src/components/ranking.jsx
@@ -1,8 +1,7 @@
 import { useEffect, useState } from "react";
 import RankingFilter from "./rankingsFilter";
 import RankingsTable from "./rankingsTable";
-import { Alert } from "@chakra-ui/react";
-import { Provider } from "./ui/provider";
+import ChakraAlert from "./chakraAlert";
 
 export default function RankingsPage() {
   const [filterParams, setFilterParams] = useState({
@@ -20,11 +19,7 @@ export default function RankingsPage() {
       <h1 className="mb-6 border-b md:text-center pb-2 text-3xl font-semibold tracking-tight first:mt-0">
         Rankings
       </h1>
-      <Provider>
-        <Alert.Root className="my-6">
-          <Alert.Title>This ranking considers only the competitions organized by SCI. If you want to get listed in this ranking, please attend a competition organized by SCI. If you have aleady participated in any SCI competition and if your name is not there in the ranking list, please claim your state by sending an email to <a className="hover:underline hover:underline-offset-2" href="mailto:speedcubersindiadata@gmail.com" target="_blank" rel="noopener noreferrer">speedcubersindiadata@gmail.com</a> by attaching your ID proof.</Alert.Title>
-        </Alert.Root>
-      </Provider>
+      <ChakraAlert/>
       <RankingFilter setFilterParams={setFilterParams} />
       <RankingsTable filterParams={filterParams} />
     </div>
--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,15 @@ import App from "./App.jsx";
 import "./index.css";
 import { NextUIProvider } from "@nextui-org/react";
 import { ThemeProvider } from "@material-tailwind/react";
+import { Provider } from "./components/ui/provider.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <ThemeProvider>
       <NextUIProvider>
+        <Provider>
           <App />
+        </Provider>
       </NextUIProvider>
     </ThemeProvider>
   </StrictMode>

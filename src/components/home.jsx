00857d973baf4defc937127a4c1a46a7d6a1d994
Allen John <146406094+allenjohn07@users.noreply.github.com>
2025-06-06 21:49:33 +0530

    change: Sub Heading (#21)

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -12,8 +12,8 @@ export const HomeComponent = () => {
     <div className="container max-w-screen-lg mx-auto px-4 md:px-8 py-8 md:py-14 space-y-5 md:space-y-10 text-blue-gray-900">
       <h2 className="scroll-m-20 border-b pb-2 text-3xl text-center font-semibold tracking-tight first:mt-0">
         SpeedCubers India <br />
-        <span className="scroll-m-20 mb-10 border-b pb-2 text-2xl font-semibold tracking-tight first:mt-0">
-          WCA Regional Organisation
+        <span className="scroll-m-20 mb-10 pb-2 text-xl font-normal tracking-tight first:mt-0">
+          Awaiting WCA approval for being India's official RO
         </span>
       </h2>
       <div className="w-full flex justify-center">

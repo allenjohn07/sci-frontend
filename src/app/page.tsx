13dafad4cf909d5755e4df2daf466a9f82ba2581
Allen John <allenjohnmonapallil@gmail.com>
2025-10-28 11:23:00 -0600

    update: chakra ui in page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,33 +3,49 @@
 import Competitions from "@/components/competitions";
 import InstagramEmbed from "@/components/InstagramEmbed";
 import { MoreLinks } from "@/components/links";
+import { Box, Heading, Text } from "@chakra-ui/react";
 import React from "react";
-import { useEffect } from "react";
 
 export default function Home() {
-
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, []);
-
   return (
-    <div className="container max-w-screen-lg mx-auto px-4 md:px-8 py-8 md:py-14 space-y-5 md:space-y-10 text-blue-gray-900">
-          <h2 className="scroll-m-20 border-b pb-2 text-3xl text-center font-semibold tracking-tight first:mt-0">
-            SpeedCubers India <br />
-            <span className="scroll-m-20 mb-10 pb-2 text-xl font-normal tracking-tight first:mt-0">
-              Awaiting WCA approval for being India&apos;s official RO
-            </span>
-          </h2>
-          <div className="w-full flex justify-center">
-            <InstagramEmbed
-              postUrl="https://www.instagram.com/p/C4Yf-CqS7q2/"
-              caption={true}
-            />
-          </div>
-          <div>
-            <Competitions />
-          </div>
-          <MoreLinks />
-        </div>
+    <Box
+      className="container space-y-10 text-blue-gray-900"
+      maxW="screen-lg"
+      mx="auto"
+      px={{ base: 4, md: 8 }}
+      py={{ base: 10, md: 14 }}
+    >
+      <Heading
+        as="h2"
+        scrollMargin="20"
+        fontSize="3xl"
+        textAlign="center"
+        fontWeight="semibold"
+        letterSpacing="tight"
+        _first={{ mt: 0 }}
+      >
+        SpeedCubers India <br />
+        <Text
+          as="span"
+          scrollMargin="20"
+          fontSize="xl"
+          fontWeight="normal"
+          letterSpacing="tight"
+          _first={{ mt: 0 }}
+        >
+          Awaiting WCA approval for being India&apos;s official RO
+        </Text>
+      </Heading>
+      <Box w="full" display="flex" justifyContent="center">
+        <InstagramEmbed
+          postUrl="https://www.instagram.com/p/C4Yf-CqS7q2/"
+          caption={true}
+        />
+      </Box>
+      <Box>
+        <Competitions />
+      </Box>
+      <MoreLinks />
+    </Box>
   );
 }

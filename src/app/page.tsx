b3adf7060f4f14a00ab99df27732e907b1b719e0
Allen John <146406094+allenjohn07@users.noreply.github.com>
2025-10-28 20:47:22 -0600

    cleanup: home & homeHolder (#24)
    * cleanup: home & homeHolder
    
    * update: chakra ui in page.tsx
    
    * add: new line

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,51 @@
+"use client";
+
+import Competitions from "@/components/competitions";
+import InstagramEmbed from "@/components/InstagramEmbed";
+import { MoreLinks } from "@/components/links";
+import { Box, Heading, Text } from "@chakra-ui/react";
 import React from "react";
-import HomeComponent from "../components/homeHolder";
 
 export default function Home() {
-  return <HomeComponent />;
+  return (
+    <Box
+      className="container space-y-10 text-blue-gray-900"
+      maxW="screen-lg"
+      mx="auto"
+      px={{ base: 4, md: 8 }}
+      py={{ base: 10, md: 14 }}
+    >
+      <Heading
+        as="h2"
+        scrollMargin="20"
+        fontSize="3xl"
+        textAlign="center"
+        fontWeight="semibold"
+        letterSpacing="tight"
+        _first={{ mt: 0 }}
+      >
+        SpeedCubers India <br />
+        <Text
+          as="span"
+          scrollMargin="20"
+          fontSize="xl"
+          fontWeight="normal"
+          letterSpacing="tight"
+          _first={{ mt: 0 }}
+        >
+          Awaiting WCA approval for being India&apos;s official RO
+        </Text>
+      </Heading>
+      <Box w="full" display="flex" justifyContent="center">
+        <InstagramEmbed
+          postUrl="https://www.instagram.com/p/C4Yf-CqS7q2/"
+          caption={true}
+        />
+      </Box>
+      <Box>
+        <Competitions />
+      </Box>
+      <MoreLinks />
+    </Box>
+  );
 }
